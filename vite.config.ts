72af665219e98cf3628fe63274ee0f3f0ed40a4b
jkarenko <juho.karenko@gmail.com>
2025-04-26 18:17:24 +0300

    chore(config): update base and start_url for PWA deployment
    - Set base path to '/kirjoitusharjoitus/' for proper routing in the PWA.
    - Adjust start_url to './' to ensure correct loading of the application.
    
    These changes facilitate the deployment of the PWA with the correct configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/kirjoitusharjoitus/',
   plugins: [
     VitePWA({
       strategies: 'injectManifest',
@@ -19,7 +20,7 @@ export default defineConfig({
         background_color: '#f5f7fa',
         display: 'standalone',
         orientation: 'any',
-        start_url: '/',
+        start_url: './',
         icons: [
           {
             src: 'pwa-192x192.png',
